93164abcbb8ca950de9c1272a3237197e652c22d
Святослав Корелин <114075953+SvyatoslavKorelin@users.noreply.github.com>
2022-09-29 13:12:46 +0300

    Delete ПрограммированиенаязыкеPython directory

--- ПрограммированиенаязыкеPython/23 сентября/2.py
+++
@@ -1,6 +0,0 @@
-a = input().split(", ")
-n = ["5","4","3"]
-usp = 0
-for elem in n:
-    usp += a.count(elem)
-print(usp / len(a) * 100)
--- ПрограммированиенаязыкеPython/5.py
+++
@@ -1,4 +0,0 @@
-a = 4
-b = 10
-c = max(a, b) - min(a,b)
-print(c)
