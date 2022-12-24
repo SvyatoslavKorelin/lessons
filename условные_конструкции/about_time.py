79177f2365e2b065672681893d838c6c32dd03e9
Святослав Корелин <114075953+SvyatoslavKorelin@users.noreply.github.com>
2022-12-24 09:28:53 +0300

    Add files via upload

---
+++ условные_конструкции/about_time.py
@@ -0,0 +1,16 @@
+price = input("цена")
+time = input("время (час)")
+a = price//2
+b = price//4
+if time in range(10, 12):
+    print(a)
+if time in range(20, 22):
+    print(b)
+else:
+    print(price)
+
+
+
+
+
+
